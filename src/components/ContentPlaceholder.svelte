dd0b12289bc3146efaca2cfb18d4a06f4d4ea63e
Shayan <klaus_hybrid33@yahoo.com>
2022-12-13 01:09:03 +0330

    added content placeholder

---
+++ src/components/ContentPlaceholder.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let hidden = true;
+    import { servers } from "../stores"
+    if (servers.subscribe(servers => servers.length > 0)) {
+        hidden = false;
+    }
+</script>
+
+{#if !hidden}
+<div class="container mx-auto">
+  <div class="flex items-center justify-center py-10">
+    <p class="text-gray-600 font-light text-center md:text-left lg:text-center">
+      Add a server by clicking the + to the right. -->
+    </p>
+  </div>
+</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-
+import ContentPlaceholder from "../components/ContentPlaceholder.svelte";
 </script>
 
 <main class="max-w-full">
+	<ContentPlaceholder />
 	<div class="p-8 md:p-24 lg:p-32 mx-auto text-center max-w-full">
 	</div>
 	<div class="mx-auto max-w-full mt-64 lg:mt-0">
