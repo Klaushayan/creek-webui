c86bf74da2c94511aa2e1ec7dcf230f8eaf5b5e9
Shayan <klaus_hybrid33@yahoo.com>
2022-12-13 01:08:42 +0330

    expanded form input and refactored

--- src/components/FormInput.svelte
+++ src/components/FormInput.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  export let inputLabel: string = "Default Label";
-  export let placeholder: string = inputLabel;
-  export let inputType: string = "text";
+  export let label: string = "Default Label";
+  export let placeholder: string = label;
+  export let type: string = "text";
   export let errorMessage: string = "Wrong input";
   export let validation: Function = () => true;
+  export let name: string = "defaultName";
+  export let value: string = "";
 
   // TODO: check this for performance
   export let id: string = Math.random().toString(36).substring(7);
@@ -23,22 +25,23 @@
       input.classList.add("border-red-500");
       visibility = "block";
     }
-
   }
 </script>
 
 <label
   class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-  for="{id}"
+  for={id}
 >
-  {inputLabel}
+  {label}
 </label>
 <input
   class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-  id="{id}"
-  type={inputType}
-  placeholder={placeholder}
-  on:input="{onInput}"
+  {id}
+  {name}
+  {type}
+  {placeholder}
+  {value}
+  on:input={onInput}
 />
 
 <p class="{visibility} text-red-500 text-s">{errorMessage}</p>
