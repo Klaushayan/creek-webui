c5595c95ae1f902c478baa31555117d9c2bccf1c
Shayan <klaus_hybrid33@yahoo.com>
2022-12-23 02:45:02 +0330

    wip

---
+++ src/components/AddServer.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import FormInput from "./FormInput.svelte";
+
+</script>
+
+<div
+  class="
+    flex items-center justify-center py-10
+"
+>
+    <FormInput
+      label="Server Name"
+      placeholder="Server Name"
+      type="text"
+      name="serverName"
+      id="serverName"
+      value=""
+    />
+</div>
